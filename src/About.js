2e01ce708756f02ae273fbdda80f5c42ca971a7c
JMurkyHawk <jmiaguard-github@yahoo.com>
2024-02-07 02:19:54 -0600

    Add updates from react router hooks and links tutorial

--- src/About.js
+++ src/About.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const About = () => {
     return (
-        <main>
-            <h1>About</h1>
+        <main className="About">
+            <h2>About</h2>
+            <p style={{ margin: "1rem 0 0 0" }}>This blog is a project in the Learn React tutorial series.</p>
         </main>
     )
 }
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Feed from './Feed';
 
-const Home = () => {
+const Home = ({ posts }) => {
     return (
-        <main>
-            <h1>Home</h1>
+        <main className="Home">
+            {posts.length ? (
+                <Feed posts={posts} />
+            ) : (
+                <p style={{ magin: "2rem 0 0 0"}}>
+                    No posts to display.
+                </p>
+            )} 
         </main>
     )
 }
