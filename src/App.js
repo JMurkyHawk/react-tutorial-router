2d877fddfeeb58621798b8e166103b976990ffd0
JMurkyHawk <jmiaguard-github@yahoo.com>
2024-02-12 09:18:41 -0600

    React state management with Redux easy-peasy api

--- src/App.js
+++ src/App.js
@@ -7,25 +7,36 @@ import PostPage from './PostPage';
 import EditPost from './EditPost';
 import About from './About';
 import Missing from './Missing';
+import { useEffect } from 'react';
+import useAxiosFetch from './hooks/useAxiosFetch';
 import { Route, Switch } from 'react-router-dom';
-import { DataProvider } from './context/DataContext';
+import { useStoreActions } from 'easy-peasy';
 
 function App() {
+    const setPosts = useStoreActions((actions) => actions.setPosts);
+    const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
+
+    useEffect(() => {
+        setPosts(data);
+    }, [data, setPosts]);
 
     return (
         <div className="App">
             <Header title="React JS Blog" />
-            <DataProvider>
-                <Nav />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/post" component={NewPost} /> 
-                    <Route path="/edit/:id" component={EditPost} />
-                    <Route path="/post/:id" component={PostPage} />
-                    <Route path="/about" component={About} />
-                    <Route path="*" component={Missing} />
-                </Switch>
-            </DataProvider>
+            <Nav />
+            <Switch>
+                <Route exact path="/">
+                    <Home 
+                        isLoading={isLoading} 
+                        fetchError={fetchError}
+                    />
+                </Route>
+                <Route exact path="/post" component={NewPost} /> 
+                <Route path="/edit/:id" component={EditPost} />
+                <Route path="/post/:id" component={PostPage} />
+                <Route path="/about" component={About} />
+                <Route path="*" component={Missing} />
+            </Switch>
             <Footer />
         </div>
     );
